0a6826db37918c24c61ff7310f2484bd99138795
danielaordonez <danii-om@hotmail.com>
2017-06-11 16:25:36 -0600

    Listo

--- src/uvframework/models/EstudiantesModel.java
+++ src/uvframework/models/EstudiantesModel.java
@@ -7,11 +7,7 @@ package uvframework.models;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import uvframework.models.entities.EstudianteEntity;
 import uvframework.tools.MySQLConn;
